a5fbf92fdeb697226c3c4e5ab06a8fe86a24b493
Eric Suescun <edsuescun@gmail.com>
2019-10-11 16:44:47 -0500

    Se modifican las rutas para los controlladores del Devise para ambos modelos User y Admin.

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,10 @@ Rails.application.routes.draw do
 	resources :users
 	resources :admins
 
-	devise_for :users, path: 'users'
-	devise_for :admins, path: 'admins'
+	devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions", registrations: "admins/registrations", passwords: "admins/passwords", unlocks: "admins/unlocks", confirmations: "admins/confirmations" }
+
+	devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations", passwords: "users/passwords", unlocks: "users/unlocks", confirmations: "users/confirmations" }
+	
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

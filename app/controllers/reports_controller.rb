82ce0dc81d1147885f7146266213363ef7dbd63e
Eric Suescun <edsuescun@gmail.com>
2019-10-29 19:40:45 -0500

    Se montan los reportes con fechas de inicio y fin.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,11 +1,12 @@
 class ReportsController < ApplicationController
   def status
-  	date_range = (Date.today - 7).beginning_of_day..Date.today.end_of_day
+
+  	date_range = Date.parse(params[:init_date]).beginning_of_day..Date.parse(params[:final_date]).end_of_day
   	@informs = Inform.where(created_at: date_range)
   end
 
   def objections
-  	date_range = (Date.today - 7).beginning_of_day..Date.today.end_of_day
+  	date_range = Date.parse(params[:init_date]).beginning_of_day..Date.parse(params[:final_date]).end_of_day
   	@objections = Objection.where(created_at: date_range)
   end
 

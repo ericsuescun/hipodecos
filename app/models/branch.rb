b776005f46320764ae83ca619d2b0e132e9a73fe
Eric Suescun <edsuescun@gmail.com>
2019-11-07 19:32:15 -0500

    Se elimina admin_id del importador de Branch porque el modelo NO lo tiene.

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -6,7 +6,7 @@ class Branch < ApplicationRecord
 
   def self.import(file)
   	CSV.foreach(file.path, :col_sep => (";"), headers: true, encoding: 'iso-8859-1:utf-8') do |row|
-  		Branch.create!(row.to_hash.merge(admin_id: 1))
+  		Branch.create!(row.to_hash)
   	end
   end
 

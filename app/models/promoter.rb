154c41d6937bacaab6750c72845a900ad0474ffc
Eric Suescun <edsuescun@gmail.com>
2019-11-07 19:09:50 -0500

    Se elimina admin_id del importador de Promoter porque el modelo NO lo tiene.

--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -4,7 +4,7 @@ class Promoter < ApplicationRecord
 
 	def self.import(file)
 		CSV.foreach(file.path, :col_sep => (";"), headers: true, encoding: 'iso-8859-1:utf-8') do |row|
-			Promoter.create!(row.to_hash.merge(admin_id: 1))
+			Promoter.create!(row.to_hash)
 		end
 	end
 end

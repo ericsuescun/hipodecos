3186085e23d10646586c0d13e76f378c85839ce3
Eric Suescun <edsuescun@gmail.com>
2020-05-02 01:08:10 -0500

    Se ejecuta migration del modelo Template

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_015435) do
+ActiveRecord::Schema.define(version: 2020_05_02_060405) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -369,6 +369,14 @@ ActiveRecord::Schema.define(version: 2020_04_28_015435) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["inform_id"], name: "index_studies_on_inform_id"
+  end
+
+  create_table "templates", force: :cascade do |t|
+    t.string "organ"
+    t.string "title"
+    t.string "template_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|

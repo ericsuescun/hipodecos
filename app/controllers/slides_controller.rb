b0e00c0e64d072199cb37c89768e000e2706e3fb
Eric Suescun <edsuescun@gmail.com>
2020-05-20 08:15:38 -0500

    Se incluyen los id dentro del group clause para slide y block para el postgre

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -9,9 +9,9 @@ class SlidesController < ApplicationController
       initial_date = Date.new(params[:yi].to_i, params[:mi].to_i, params[:di].to_i).beginning_of_day
       final_date = Date.new(params[:yf].to_i, params[:mf].to_i, params[:df].to_i).end_of_day
       date_range = initial_date..final_date
-      @slides = Slide.where(created_at: date_range).select(:id, :inform_id, :user_id, :created_at, :description, :slide_tag, :colored, :tagged, :covered, :stored).group(:inform_id)
+      @slides = Slide.where(created_at: date_range).select(:id, :inform_id, :user_id, :created_at, :description, :slide_tag, :colored, :tagged, :covered, :stored).group(:id, :inform_id)
     else
-      @slides = Slide.all.select(:id, :inform_id, :user_id, :created_at, :description, :slide_tag, :colored, :tagged, :covered, :stored).group(:inform_id)
+      @slides = Slide.all.select(:id, :inform_id, :user_id, :created_at, :description, :slide_tag, :colored, :tagged, :covered, :stored).group(:id, :inform_id)
     end
   end
 

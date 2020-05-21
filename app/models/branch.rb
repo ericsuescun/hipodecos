918cc91552fb90fed8c756eb7f1451c93c7a504d
Eric Suescun <edsuescun@gmail.com>
2020-05-21 10:56:59 -0500

    Se hace fix de default scope para promoter y branch con order por initials

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -4,6 +4,8 @@ class Branch < ApplicationRecord
   validates :name, :initials, :code, presence: true
   validates :code, uniqueness: true
 
+  default_scope -> { order(initials: :asc) }
+
   def self.import(file)
   	CSV.foreach(file.path, :col_sep => (";"), headers: true, encoding: 'iso-8859-1:utf-8') do |row|
   		Branch.create!(row.to_hash)
--- app/models/promoter.rb
+++ app/models/promoter.rb
@@ -2,7 +2,7 @@ class Promoter < ApplicationRecord
 	validates :name, :initials, :code, presence: true
 	validates :code, uniqueness: true
 
-	default_scope -> { order(name: :asc)}
+	default_scope -> { order(initials: :asc)}
 
 	def self.import(file)
 		CSV.foreach(file.path, :col_sep => (";"), headers: true, encoding: 'iso-8859-1:utf-8') do |row|

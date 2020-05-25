957b007082b8c4cd2ffec46f5fb7e12ed43f7a92
Eric Suescun <edsuescun@gmail.com>
2020-05-25 14:02:10 -0500

    Se hace el fix para que al cargar Coloreado y no haya filtro de fecha, escoja todas las slides, el que no haya filtro de fecha significa que yi pueda ser una cadena vacia

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -113,7 +113,7 @@ class SlidesController < ApplicationController
   def color
     # Slide.update_all( { colored: true }, { id: params[:slide_ids] } )
     Slide.where(id: params[:slide_ids]).update_all({colored: true})
-    if params[:yi]
+    if params[:yi] != ""
       redirect_to processing_slides_coloring_slides_path + "?di=#{params[:di]}&mi=#{params[:mi]}&yi=#{params[:yi]}&df=#{params[:df]}&mf=#{params[:mf]}&yf=#{params[:yf]}"
     else
       redirect_to processing_slides_coloring_slides_path

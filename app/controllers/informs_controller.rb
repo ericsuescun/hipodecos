dd78f6a0ba6b8f54166b03f6bd79a491aee382ac
Eric Suescun <edsuescun@gmail.com>
2020-05-29 23:54:01 -0500

    Se especifica el unscoped en descr micro para evitar que elpostgres pida el campo de ordenamiento.

--- app/controllers/informs_controller.rb
+++ app/controllers/informs_controller.rb
@@ -34,10 +34,10 @@ class InformsController < ApplicationController
     @organs = Organ.all
 
     @automatics = []
-    @inform.samples.select(:organ_code).distinct.each do |sample|
+    @inform.samples.unscoped.select(:organ_code).distinct.each do |sample|
       Automatic.where(auto_type: "micro", organ: sample.organ_code).each do |auto|
         @automatics << auto
-    end
+      end
     end   
 
     @samples = @inform.samples

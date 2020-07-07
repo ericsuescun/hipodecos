e9f66ac6b19574377df9e704831e7f34e39553d0
Eric Suescun <edsuescun@gmail.com>
2020-07-07 15:02:30 -0500

    Se incluye el wkhtmltopdf-binary gem en el gem file

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'pdfkit'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,6 +16,8 @@ module Hipodecos
 
     config.i18n.default_locale = :es
     config.i18n.enforce_available_locales = true
+
+    config.middleware.use PDFKit::Middleware, :print_media_type => true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

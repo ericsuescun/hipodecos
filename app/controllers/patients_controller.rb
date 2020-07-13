5f7629c012a45632b65879be7dff93362be7c53e
Eric Suescun <edsuescun@gmail.com>
2020-07-13 15:50:37 -0500

    Se eliminan parentesis de consecutive en la accion create de patient

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -91,14 +91,14 @@ class PatientsController < ApplicationController
       if params[:patient][:informs_attributes][:"0"][:inf_type] == "clin"
         #@patient.informs.first.update(tag_code: ('C' + Date.today.strftime('%y').to_s + '-' + (@patient.informs.first.id - finf.id + 1).to_s)) #Para aplicar el consecutivo, tomo el id del registro actual, le resto el del primero del año y le sumo 1. Si coinciden, es decir, estoy justo en el primer registro del año, la resta dará 0, y el consecutivo asignado será 1 ! Esto además tiene en cuenta todos los registros pues esta basado en el id.
         consecutive = Inform.where(inf_type: "clin", created_at: date_range).count
-        @patient.informs.first.update(tag_code: ("C" + Date.today.strftime('%y').to_s + '-' + (consecutive).to_s))
+        @patient.informs.first.update(tag_code: ("C" + Date.today.strftime('%y').to_s + '-' + consecutive.to_s))
       else
         if params[:patient][:informs_attributes][:"0"][:inf_type] == "hosp"
           consecutive = Inform.where(inf_type: "hosp", created_at: date_range).count
-          @patient.informs.first.update(tag_code: ("H" + Date.today.strftime('%y').to_s + '-' + (consecutive).to_s))
+          @patient.informs.first.update(tag_code: ("H" + Date.today.strftime('%y').to_s + '-' + consecutive.to_s))
         else
           consecutive = Inform.where(inf_type: "cito", created_at: date_range).count
-          @patient.informs.first.update(tag_code: ("K" + Date.today.strftime('%y').to_s + '-' + (consecutive).to_s))
+          @patient.informs.first.update(tag_code: ("K" + Date.today.strftime('%y').to_s + '-' + consecutive.to_s))
         end #La instancia de inform hace que se el conteo COUNT de +1. Por eso consecutive no le sumo 1
       end
       redirect_to inform_path(@patient.informs.first), notice: 'Paciente matriculado exitosamente.'

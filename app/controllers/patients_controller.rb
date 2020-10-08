674f39f14d1f0dd361b9855208afae8ffea04112
Eric Suescun <edsuescun@gmail.com>
2020-10-07 19:22:53 -0500

    Se el partial de inform se envía el id de informe en el link de edición de Patient. En el Patient controller se busca el id del informe para poder retornar justo a la vista show revision de dicho informe, si es que el parámetro esta. El parámetro se pasa a la form de patient y esta a su vez lo pasa como hidden attribute a la update action en el controller para poder retornar adecuadamente.

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -195,7 +195,18 @@ class PatientsController < ApplicationController
 
   def update
     if @patient.update(patient_params)
-      redirect_to @patient, notice: 'Patient was successfully updated.'
+      if params[:patient][:inform] != nil
+        inform = Inform.find(params[:patient][:inform])
+        if inform.inf_status == "revision"
+          redirect_to show_revision_inform_path(inform), notice: 'Informe exitosamente actualizado.'
+        else
+          redirect_to @patient, notice: 'Paciente exitosamente editado.'
+        end
+      else
+        redirect_to @patient, notice: 'Paciente exitosamente editado.'
+      end
+      
+      
     else
       render :edit
     end

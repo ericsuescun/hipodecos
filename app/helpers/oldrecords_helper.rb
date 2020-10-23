c1f493e5ad3084e2da4da517fbffd231f1649c55
Eric Suescun <edsuescun@gmail.com>
2020-10-23 07:20:13 -0500

    Se aplica fix_accent a vista show de oldrecord

--- app/helpers/oldrecords_helper.rb
+++ app/helpers/oldrecords_helper.rb
@@ -1,2 +1,16 @@
 module OldrecordsHelper
+	def fix_accent(descr)
+		
+		descr.gsub!("Ã\u009F","á")
+		
+		descr.gsub!("Ã\u009A","é")
+		
+		descr.gsub!("Ã\u009D","í")
+
+		descr.gsub!("Â¾","ó")
+
+		descr.gsub!("Ã\u008B","Ó")
+		
+		return descr
+	end
 end

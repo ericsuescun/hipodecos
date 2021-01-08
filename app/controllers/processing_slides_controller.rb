a0d4da29650746116b0ff43fcfe037f314ba53d7
Eric Suescun <ericsuescun@MacBook-Air-de-Eric.local>
2021-01-07 22:53:12 -0500

    Se modifica el controlador de procesing slides para filtrar por clin hosp y cito. Se agrega en la parte superior de las vistas de procesing slides el tipo de informe para las placas.

--- app/controllers/processing_slides_controller.rb
+++ app/controllers/processing_slides_controller.rb
@@ -28,9 +28,36 @@ class ProcessingSlidesController < ApplicationController
 		  date_range = initial_date..final_date
 		end
 
-		@slidesc = Slide.where(created_at: date_range, colored: true)
-		@slides_first_batch = Slide.where(created_at: date_range, colored: false).limit(30)
-		@slides_rest = Slide.where(created_at: date_range, colored: false).offset(30)
+		# @slidesc = Slide.where(created_at: date_range, colored: true)
+		@slidesc = []
+		Slide.where(created_at: date_range, colored: true).each do |slide|
+			if params[:inf_type] == "clin" || params[:inf_type] == "hosp"
+				@slidesc << slide if slide.slide_tag[0] == 'C' || slide.slide_tag[0] == 'H'
+			else
+				@slidesc << slide if slide.slide_tag[0] == 'K'
+			end
+			
+		end
+
+		# @slides_first_batch = Slide.where(created_at: date_range, colored: false).limit(30)
+		@slidesnc = []
+		Slide.where(created_at: date_range, colored: false).each do |slide|
+			if params[:inf_type] == "clin" || params[:inf_type] == "hosp"
+				@slidesnc << slide if slide.slide_tag[0] == 'C' || slide.slide_tag[0] == 'H'
+			else
+				@slidesnc << slide if slide.slide_tag[0] == 'K'
+			end
+		end
+		@slides_first_batch = @slidesnc[0..29]
+		if @slides_first_batch == nil
+			@slides_first_batch = []
+		end
+
+		# @slides_rest = Slide.where(created_at: date_range, colored: false).offset(30)
+		@slides_rest = @slidesnc[30..-1]
+		if @slides_rest == nil
+			@slides_rest = []
+		end
 	end
 
 	def covering_slides
@@ -57,9 +84,37 @@ class ProcessingSlidesController < ApplicationController
 		  final_date = Time.now.end_of_day
 		  date_range = initial_date..final_date
 		end
-		@slidesc = Slide.where(created_at: date_range, colored: true, covered: true)
-		@slides_first_batch = Slide.where(created_at: date_range, colored: true, covered: false).limit(30)
-		@slides_rest = Slide.where(created_at: date_range, colored: true, covered: false).offset(30)
+		# @slidesc = Slide.where(created_at: date_range, colored: true, covered: true)
+		# @slides_first_batch = Slide.where(created_at: date_range, colored: true, covered: false).limit(30)
+		# @slides_rest = Slide.where(created_at: date_range, colored: true, covered: false).offset(30)
+
+		@slidesc = []
+		Slide.where(created_at: date_range, colored: true, covered: true).each do |slide|
+			if params[:inf_type] == "clin" || params[:inf_type] == "hosp"
+				@slidesc << slide if slide.slide_tag[0] == 'C' || slide.slide_tag[0] == 'H'
+			else
+				@slidesc << slide if slide.slide_tag[0] == 'K'
+			end
+			
+		end
+
+		@slidesnc = []
+		Slide.where(created_at: date_range, colored: true, covered: false).each do |slide|
+			if params[:inf_type] == "clin" || params[:inf_type] == "hosp"
+				@slidesnc << slide if slide.slide_tag[0] == 'C' || slide.slide_tag[0] == 'H'
+			else
+				@slidesnc << slide if slide.slide_tag[0] == 'K'
+			end
+		end
+		@slides_first_batch = @slidesnc[0..29]
+		if @slides_first_batch == nil
+			@slides_first_batch = []
+		end
+
+		@slides_rest = @slidesnc[30..-1]
+		if @slides_rest == nil
+			@slides_rest = []
+		end
 	end
 
 	def tagging_slides
@@ -86,8 +141,36 @@ class ProcessingSlidesController < ApplicationController
 		  final_date = Time.now.end_of_day
 		  date_range = initial_date..final_date
 		end
-		@slidesc = Slide.where(created_at: date_range, colored: true, covered: true, tagged: true)
-		@slides_first_batch = Slide.where(created_at: date_range, colored: true, covered: true, tagged: false).limit(30)
-		@slides_rest = Slide.where(created_at: date_range, colored: true, covered: true, tagged: false).offset(30)
+		# @slidesc = Slide.where(created_at: date_range, colored: true, covered: true, tagged: true)
+		# @slides_first_batch = Slide.where(created_at: date_range, colored: true, covered: true, tagged: false).limit(30)
+		# @slides_rest = Slide.where(created_at: date_range, colored: true, covered: true, tagged: false).offset(30)
+
+		@slidesc = []
+		Slide.where(created_at: date_range, colored: true, covered: true, tagged: true).each do |slide|
+			if params[:inf_type] == "clin" || params[:inf_type] == "hosp"
+				@slidesc << slide if slide.slide_tag[0] == 'C' || slide.slide_tag[0] == 'H'
+			else
+				@slidesc << slide if slide.slide_tag[0] == 'K'
+			end
+			
+		end
+
+		@slidesnc = []
+		Slide.where(created_at: date_range, colored: true, covered: true, tagged: false).each do |slide|
+			if params[:inf_type] == "clin" || params[:inf_type] == "hosp"
+				@slidesnc << slide if slide.slide_tag[0] == 'C' || slide.slide_tag[0] == 'H'
+			else
+				@slidesnc << slide if slide.slide_tag[0] == 'K'
+			end
+		end
+		@slides_first_batch = @slidesnc[0..29]
+		if @slides_first_batch == nil
+			@slides_first_batch = []
+		end
+
+		@slides_rest = @slidesnc[30..-1]
+		if @slides_rest == nil
+			@slides_rest = []
+		end
 	end
 end

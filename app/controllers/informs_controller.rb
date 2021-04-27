d5f21d78f7bdb3fdb595217307a8cfeea07664e0
Eric Suescun <edsuescun@gmail.com>
2021-04-26 19:33:10 -0500

    Se hace un fix en la exportacion de citos para los campos de notas, diag, sugerencias

--- app/controllers/informs_controller.rb
+++ app/controllers/informs_controller.rb
@@ -363,9 +363,9 @@ class InformsController < ApplicationController
 
           if params[:inf_type] == 'cito'
             file += '"' + inform.p_tel.to_s + "-" + '"' + ","
-            file += '"' + inform.diagnostics.last.result.to_s + '"' + ","
+            file += '"' + inform.micros.last.description.to_s + '"' + ","
             file += '"' + inform.diagnostics.last.description.to_s + '"' + ","
-            file += '"' + inform.cytologies.first.suggestion.to_s + '"' + ","
+            file += '"' + inform.suggestions.last.description.to_s + '"' + ","
             file += '"' + User.where(id: inform.cytologist).first.try(:fullname).to_s.upcase + '"' + ","
             if inform.diagnostics.count == 1
               file += '"' + "DAVID SUESCUN TARAZONA" + '"' + ","

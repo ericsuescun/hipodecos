5549444866b6db418f549a2e7abba5b6f8220147
Eric Suescun <ericsuescun@MacBook-Pro-de-Eric.local>
2021-10-12 15:26:39 -0500

    Se eliminan los check de Asignar en distribution cyto. Se cambian lso limites para coloreo de placas de 30 a 90

--- app/controllers/processing_slides_controller.rb
+++ app/controllers/processing_slides_controller.rb
@@ -5,23 +5,23 @@ class ProcessingSlidesController < ApplicationController
 		@tab = :color
 
 		@slidesc = []
-		@slides_first_batch = Slide.not_colored.not_covered.not_tagged.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).limit(30)
-		@slides_rest = Slide.not_colored.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).offset(30)
+		@slides_first_batch = Slide.not_colored.not_covered.not_tagged.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).limit(90)
+		@slides_rest = Slide.not_colored.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).offset(90)
 	end
 
 	def covering_slides
 		@tab = :cover
 
 		@slidesc = []
-		@slides_first_batch = Slide.colored.not_covered.not_tagged.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).limit(30)
-		@slides_rest = Slide.not_colored.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).offset(30)
+		@slides_first_batch = Slide.colored.not_covered.not_tagged.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).limit(90)
+		@slides_rest = Slide.not_colored.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).offset(90)
 	end
 
 	def tagging_slides
 		@tab = :tag
 		
 		@slidesc = []
-		@slides_first_batch = Slide.colored.covered.not_tagged.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).limit(30)
-		@slides_rest = Slide.not_colored.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).offset(30)
+		@slides_first_batch = Slide.colored.covered.not_tagged.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).limit(90)
+		@slides_rest = Slide.not_colored.joins(:inform).merge(Inform.where(inf_type: params[:inf_type])).offset(90)
 	end
 end

d2469e810a0ff36dafc0798d167a88cc1c741bc4
Eric Suescun <ericsuescun@MacBook-Pro-de-Eric.local>
2021-11-19 13:25:00 -0500

    Se transcribe el preview actual en el show de Results. También se hace el cambio en el show de controller

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -10,20 +10,78 @@ class ResultsController < ApplicationController
 	def show
 		if current_patient.id == Inform.where(id: params[:id]).first.patient.id
 				@inform = Inform.find(params[:id])
-			  	@pathologists = []
-			  	# @inform.diagnostics.each do |diagnostic|
-			   #  	@pathologists << User.where(id: diagnostic.user_id).first
+
+				@samples = @inform.samples.select(:fragment, :sample_tag, :description, :name)
+				p_role = Role.where(name: "Patologia").first.id
+				if @inform.inf_type != 'cito'
+				  @pathologists = []
+
+				  if @inform.pathologist_id != nil
+				    @pathologists << User.find(@inform.pathologist_id)
+				  else
+				    @pathologists = []
+				  end
+
+
+				  @micro_text = ""
+				  @inform.micros.each do |micro|
+				    if User.find(micro.user_id).role_id == p_role
+				      @pathologists << User.find(micro.user_id)
+				    end
+				    if micro.description.size > 500
+				      @micro_text = @micro_text + "\n\r" + "\n\r" + micro.description + "\n\r "
+				    else
+				      @micro_text = @micro_text + micro.description + " "
+				    end
+
+				  end
+
+				  @diagnostic_text = ""
+				  @inform.diagnostics.each do |diagnostic|
+				    if User.find(diagnostic.user_id).role_id == p_role
+				      @pathologists << User.find(diagnostic.user_id)
+				    end
+				    @diagnostic_text = @diagnostic_text + diagnostic.description + " "
+				  end
+				  @diagnostic_codes = @inform.diagnostics.pluck(:pss_code, :who_code).uniq
+
+				  @pathologists = @pathologists.uniq
+
+				else
+				  @pathologists = []
+
+				  if @inform.pathologist_id != nil
+				    @pathologists << User.find(@inform.pathologist_id)
+				  end
+
+				  if @inform.diagnostics != []
+				    diagnostic = @inform.diagnostics.last
+				    if User.find(diagnostic.user_id).role_id == p_role
+				      @pathologists << User.find(diagnostic.user_id)
+				    end
+				  end
+				  @pathologists = @pathologists.uniq
+
+				  @cytologist = User.where(id: @inform.cytologist).first
+
+				  if @inform.cytologies != []
+				    @cytology = @inform.cytologies.first
+				  else
+				    @cytology = nil
+				  end
+				end		
+			  	# @pathologists = []
+			  	# # @inform.diagnostics.each do |diagnostic|
+			   # #  	@pathologists << User.where(id: diagnostic.user_id).first
+			  	# # end
+			  	# @pathologists << User.where(id: @inform.pathologist_id).first if @inform.pathologist_id
+			  	# @pathologists = @pathologists.uniq
+			  	# if @inform.inf_status != "downloaded"
+			  	# 	@inform.update(inf_status: "downloaded", download_date: Time.now)
 			  	# end
-			  	@pathologists << User.where(id: @inform.pathologist_id).first if @inform.pathologist_id
-			  	@pathologists = @pathologists.uniq
-			  	if @inform.inf_status != "downloaded"
-			  		@inform.update(inf_status: "downloaded", download_date: Time.now)
-			  	end
 		else
 			render :not_permitted
 		end
-		
-
 	end
 
 	def show_oldrecord

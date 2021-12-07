b8a0b594a1c6b7feaa326bcd3f1281fb33038a2a
Eric Suescun <ericsuescun@MacBook-Pro-de-Eric.local>
2021-12-06 20:40:59 -0500

    Implementa orden en los autos de descre micro

--- app/controllers/informs_controller.rb
+++ app/controllers/informs_controller.rb
@@ -654,11 +654,11 @@ class InformsController < ApplicationController
     # @inform.samples.unscoped.select(:organ_code).distinct.each do |sample|
     Sample.unscoped.where(inform_id: @inform.id).select(:organ_code).distinct.each do |sample|
       if @inform.inf_type == "cito"
-        Automatic.where(auto_type: "cito", organ: sample.organ_code).each do |auto|
+        Automatic.where(auto_type: "cito", organ: sample.organ_code).order(:title).each do |auto|
           @automatics << auto
         end
       else
-        Automatic.where(auto_type: "micro", organ: sample.organ_code).each do |auto|
+        Automatic.where(auto_type: "micro", organ: sample.organ_code).order(:title).each do |auto|
           @automatics << auto
         end
       end

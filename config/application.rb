f05bd85b6b94c19031cbeea0da292949a14c6aab
Eric Suescun <edsuescun@gmail.com>
2023-04-30 21:33:49 -0500

    Restores rack middle ware for PDF

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'pdfkit'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,5 +19,7 @@ module Hipodecos
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # options will be passed to PDFKit.new
+    config.middleware.use PDFKit::Middleware, :print_media_type => true
   end
 end

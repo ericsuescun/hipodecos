aaaff7e37a189eb3a313d332229744d98ddcf5bb
Eric Suescun <edsuescun@gmail.com>
2023-04-30 22:45:44 -0500

    Added I18 configs to application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,14 @@ module Hipodecos
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     # options will be passed to PDFKit.new
+    config.i18n.default_locale = :es
+    config.i18n.enforce_available_locales = true
+
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
+    config.time_zone = 'Bogota'
+    config.active_record.default_timezone = :local
+
     config.middleware.use PDFKit::Middleware, :print_media_type => true
   end
 end
